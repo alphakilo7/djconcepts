94aaf202889d8294b2587550797728a6733d5ebc
m.dhanwate <m.dhanwate@splendornet.com>
2021-08-12 19:37:44 +0530

    uupdated user model

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView, TemplateView
+from django.urls import reverse_lazy
 
-from . import views
+from accounts.views import api_home, LoginView, LogoutView, RegisterView
 
+
+app_name = "accounts"
 urlpatterns = [
-    path("", views.api_home, name="home"),
+    path("", TemplateView.as_view(template_name="accounts/home.html"), name="home"),
+    path("accounts/", RedirectView.as_view(url=reverse_lazy("accounts:home")), name="base"),
+    path("accounts/login/", LoginView.as_view(), name="login"),
+    path("accounts/logout/", LogoutView.as_view(), name="logout"),
+    path("accounts/register/", RegisterView.as_view(), name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,31 @@
 from datetime import datetime
 
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView as BaseLoginView
+from django.contrib.auth.views import LogoutView as BaseLogoutView
 from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+from accounts.forms import RegistrationForm
 
 
 # Create your views here.
 def api_home(request):
     return JsonResponse({"timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
                          "message": "welcome!"})
+
+
+class RegisterView(CreateView):
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("accounts:login")
+    form_class = RegistrationForm
+
+
+class LoginView(BaseLoginView):
+    template_name = "accounts/login.html"
+    redirect_authenticated_user = True
+
+
+class LogoutView(BaseLogoutView):
+    template_name = "accounts/logout.html"
+
